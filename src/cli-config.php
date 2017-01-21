e70695dc584ddbd315d13eda991708f44c53f560
Dzung Nguyen <ntd1712@gmail.com>
2017-01-21 23:23:22 +0700

    Update cli

--- src/cli-config.php
+++ src/cli-config.php
@@ -1,50 +1,18 @@
 <?php
 
-use Doctrine\DBAL\Logging\DebugStack,
-    Doctrine\ORM\Tools\Console\ConsoleRunner,
-    Chaos\Doctrine\EntityManagerFactory;
-
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : env('APP_ENV'));
-define('BASEPATH', true);
-define('APPPATH', __DIR__ . '/application/system/');
-
-static $active_group, $db;
+define('APPPATH', __DIR__ . '/application/system' . DIRECTORY_SEPARATOR);
 require_once __DIR__ . '/application/shared/bootstrap.php';
-require APPPATH . 'config/config.php';
-require APPPATH . 'config/database.php';
 
-$config = [
-    'db' => $db[$active_group] + [
-        'driver' => $db[$active_group]['dbdriver'],
-        'user' => $db[$active_group]['username'],
-        'host' => $db[$active_group]['hostname'],
-        'dbname' => $db[$active_group]['database'],
-        'prefix' => $db[$active_group]['dbprefix'],
-        'charset' => $db[$active_group]['char_set']
-    ],
-    'orm' => [
-        'cache' => [
-            'provider' => env('CACHE_DRIVER'),
-            'file' => [
-                'directory' => APPPATH . 'cache/data/',
-                'extension' => '.' . env('COOKIE_PREFIX') . '.data'
-            ]
-        ],
-        'metadata' => [
-            'driver' => 'annotation',
-            'paths' => glob(APPPATH . 'shared/Modules/*/Entities'),
-            'simple' => false
-        ],
-        'proxy_classes' => [
-            'auto_generate' => $db[$active_group]['db_debug'] ? 2 : 0,
-            'directory' => APPPATH . 'cache/proxies/',
-            'namespace' => null,
-        ],
-        'debug' => $db[$active_group]['db_debug'],
-        'default_repository' => 'Doctrine\ORM\EntityRepository',
-        'sql_logger' => $db[$active_group]['db_debug'] ? new DebugStack : null,
+$config = glob(__DIR__ . '/application/shared/Modules/*/settings.yml', GLOB_NOSORT);
+array_unshift($config, __DIR__ . '/application/shared/Modules/settings.yml');
+
+$config['__options__'] = [
+    'cache_path' => APPPATH . 'cache',
+    'replacements' => [
+        'base_path' => rtrim(APPPATH, DIRECTORY_SEPARATOR),
+        'base_url' => ''
     ]
 ];
 
-$entityManager = (new EntityManagerFactory)->setConfig($config)->getEntityManager();
-return ConsoleRunner::createHelperSet($entityManager);
+$entityManager = (new Chaos\Doctrine\EntityManagerFactory)->setConfig($config)->getEntityManager();
+return Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
