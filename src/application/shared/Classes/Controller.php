5fd7b5eab8c2ce098e93f5e26ae13d84ab3fe7f7
Dzung Nguyen <ntd1712@gmail.com>
2017-01-24 00:28:17 +0700

    Update Controller

--- src/application/shared/Classes/Controller.php
+++ src/application/shared/Classes/Controller.php
@@ -19,7 +19,7 @@ abstract class Controller extends AbstractCodeIgniterRestController
         array_unshift($config, __DIR__ . '/../Modules/settings.yml');
 
         $config['__options__'] = [
-            'cache' => false,
+            'cache' => 'production' === env('APP_ENV'),
             'cache_path' => APPPATH . 'cache',
             'replacements' => [
                 'base_path' => rtrim(APPPATH, DIRECTORY_SEPARATOR),
